dd743054adda212ce36ea6e9540e46b1768e928f
stefan <stefanantic7@gmail.com>
2018-05-15 18:22:02 +0200

    Get key and secret from env

--- config/cint.php
+++ config/cint.php
@@ -4,6 +4,6 @@ return [
     'sandbox' => (app()->environment()=='production' || app()->environment()=='prod')?false:true,
     'url_live' => 'http://api.cint.com/',
     'url_sandbox' => 'http://cdp.cintworks.net',
-    'sandbox_key' => 'f4bb1cc4-f3b1-4d04-a74a-3144029938e0',
-    'sandbox_secret' => 'kDKkaEpOu8Oad'
+    'sandbox_key' => env('sandbox_key', ''),
+    'sandbox_secret' => env('sandbox_secret', '')
 ];
